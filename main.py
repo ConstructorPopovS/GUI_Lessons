050ac7e792894531972a0fb6c4c67f9263eb238b
Simeon <popovso1999@gmail.com>
2023-05-17 11:55:05 +0100

    3 plots are working ontime. Prog Closing is failed

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import serial
+import random
 
 LARGE_FONT = ("Verdana", 18)
 style.use("ggplot")# ggplot, dark_background
@@ -31,36 +32,29 @@ fig, axs = plt.subplots(3)
 fig.set_figwidth(26)
 fig.set_figheight(14)
 
-x0_List = []
-x1_List = []
-x2_List = []
+x_List = []
 
 y0_List = []
 y1_List = []
 y2_List = []
 
-# ax.plot(x, y)
-# ax.set_title("A single plot")
+ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600,timeout=15)
 
-# A figure with two subplots
-# fig_tc, ax_tc = plt.subplots(2)
-# ax_tc[0].plot(x, y)
-# ax_tc[1].plot(x, -y)
-# ax_tc[0].set_title("The first plot")
-# ax_tc[1].set_title("The second plot")
-
-ser = serial.Serial('/dev/ttyACM0',9600)
 
 def animate(i,axs, x_List, y_List, ser, command_to_send_in_serial): 
     # command_to_send_in_serial should be in byte format: (a_tc, y_tc_List, b'command_to_send', ser)
     ser.write(command_to_send_in_serial)
-    arduinoData_string = ser.readline().decode('ascii')
+    aData_str = ser.readline().decode('ascii')
+    # aData_str = str(random.randrange(20,40))
 
     try:
-        arduinoData_float = float(arduinoData_string)
-        y_List.append(arduinoData_float)
-        print(command_to_send_in_serial.decode() + " = "+ str(arduinoData_float))
-        x_List.append(i)
+        aData_float = float(aData_str)
+        y_List.append(aData_float)
+        print(command_to_send_in_serial.decode() + " = "+ str(aData_float))
+        # TODO:Check append(i) in first time
+        if (i > 0):
+            if (x_List[-1] != i):
+                x_List.append(i)
 
     except:
         print("UserExeption: convertation tc_str to float is failed")
@@ -79,82 +73,84 @@ def animate(i,axs, x_List, y_List, ser, command_to_send_in_serial):
     axs.set_title("Thermocouple " + command_to_send_in_serial.decode())
     axs.set_ylabel("Temperature, deg C")
 
-def full_animation(i, axs1, axs2, x_List1, x_List2, y_List1, y_List2, ser, command1_to_send_in_serial, command2_to_send_in_serial):
-    animate(i,axs1, x_List1, y_List1, ser, command1_to_send_in_serial)
-    animate(i,axs2, x_List2, y_List2, ser, command2_to_send_in_serial)
+def full_animation(i, axs0, axs1, axs2, x_List, y0_List, y1_List, y2_List, ser, ser_command0, ser_command1, ser_command2):
+    animate(i, axs0, x_List, y0_List, ser, ser_command0)
+    animate(i, axs1, x_List, y1_List, ser, ser_command1)
+    animate(i, axs2, x_List, y2_List, ser, ser_command2)
 
-def animate_all_plots(i,ser, 
-                      ax0, x0_List, y0_List, command0_to_send_in_serial,
-                      ax1, x1_List, y1_List, command1_to_send_in_serial,
-                      ax2, x2_List, y2_List, command2_to_send_in_serial): 
+def animate_all_plots(i,ser, x_List,
+                      ax0, y0_List, ser_command0,
+                      ax1, y1_List, ser_command1,
+                      ax2, y2_List, ser_command2): 
     
     # command_to_send_in_serial should be in byte format: b'command_to_send'
-    ser.write(command0_to_send_in_serial)
+    ser.write(ser_command0)
     y0_str = ser.readline().decode('ascii')
 
-    ser.write(command1_to_send_in_serial)
+    ser.write(ser_command1)
     y1_str = ser.readline().decode('ascii')
 
-    ser.write(command2_to_send_in_serial)
+    ser.write(ser_command2)
     y2_str = ser.readline().decode('ascii')
+
+    
+    # y0_str = str(random.randrange(20,40))
+    # y1_str = str(random.randrange(18,35))
+    # y2_str = str(random.randrange(15,25))
 
     try:
         y0_float = float(y0_str)
         y0_List.append(y0_float)
-        print(command0_to_send_in_serial.decode() + " = "+ str(y0_float))
+        print(ser_command0.decode() + " = "+ str(y0_float))
 
         y1_float = float(y1_str)
         y1_List.append(y1_float)
-        print(command0_to_send_in_serial.decode() + " = "+ str(y1_float))
+        print(ser_command0.decode() + " = "+ str(y1_float))
         
         y2_float = float(y2_str)
         y2_List.append(y2_float)
-        print(command0_to_send_in_serial.decode() + " = "+ str(y2_float))
+        print(ser_command0.decode() + " = "+ str(y2_float))
         print("=========================")
 
-        x0_List.append(i)
-        x1_List.append(i)
-        x2_List.append(i)
+        x_List.append(i)
 
     except:
         print("UserExeption: convertation tc_str to float is failed")
+
+    x_List = x_List[-10:]
 
     y0_List = y0_List[-10:]
     y1_List = y1_List[-10:]
     y2_List = y2_List[-10:]
     
-    x0_List = x0_List[-10:]
-    x1_List = x1_List[-10:]
-    x2_List = x2_List[-10:]
-
     ax0.clear()
-    ax0.plot(x0_List,y0_List)
-    ax0.set_ylim(20, 55)
+    ax0.plot(x_List,y0_List)
+    ax0.set_ylim(10, 55)
 
     ax1.clear()
-    ax1.plot(x1_List,y1_List)
-    ax1.set_ylim(20, 55)
+    ax1.plot(x_List,y1_List)
+    # ax1.set_ylim(10, 55)
 
     ax2.clear()
-    ax2.plot(x2_List,y2_List)
-    ax2.set_ylim(20, 55)
+    ax2.plot(x_List,y2_List)
+    # ax2.set_ylim(10, 55)
 
     try:
-        ax0.set_xlim(x0_List[-10], (x0_List[-10] + 10))
-        ax1.set_xlim(x1_List[-10], (x1_List[-10] + 10))
-        ax2.set_xlim(x2_List[-10], (x2_List[-10] + 10))
+        ax0.set_xlim(x_List[-10], (x_List[-10] + 10))
+        ax1.set_xlim(x_List[-10], (x_List[-10] + 10))
+        ax2.set_xlim(x_List[-10], (x_List[-10] + 10))
     except:
         ax0.set_xlim(0, 10)
         ax1.set_xlim(0, 10)
         ax2.set_xlim(0, 10)
 
-    ax0.set_title("Thermocouple " + command0_to_send_in_serial.decode())
+    ax0.set_title("Thermocouple " + ser_command0.decode())
     ax0.set_ylabel("Temperature, deg C")
 
-    ax1.set_title("Thermocouple " + command1_to_send_in_serial.decode())
+    ax1.set_title("Thermocouple " + ser_command1.decode())
     ax1.set_ylabel("Temperature, deg C")
 
-    ax2.set_title("Thermocouple " + command2_to_send_in_serial.decode())
+    ax2.set_title("Thermocouple " + ser_command2.decode())
     ax2.set_ylabel("Temperature, deg C")
 
 class MyApp(tk.Tk):
@@ -172,6 +168,7 @@ class MyApp(tk.Tk):
         self.frames = {}
         
         for F in (StartPage, PageOne, PageTwo, PageThree):
+        # for F in (PageThree):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -257,7 +254,6 @@ class PageThree(tk.Frame):
         button3 = tk.Button(self, text="Visit page Two",
                             command=lambda: controller.show_frame(PageOne))
         button3.pack()
-        print("Page Three init")
         
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.draw()
@@ -268,18 +264,14 @@ class PageThree(tk.Frame):
 
 app = MyApp()
 MAX_FRAMES = 60
-# ani_0 = animation.FuncAnimation(fig, animate, frames=100, fargs=(axs[0], x0_List, y0_List, ser, b'tc0'), interval=1000) #, save_count=MAX_FRAMES
-# ani_1 = animation.FuncAnimation(fig, full_animation, frames=100, fargs=(axs[0], axs[1], x0_List, x1_List, y0_List, y1_List, ser, b'tc0', b'tc1'), interval=1000) #, save_count=MAX_FRAMES
+# ani_0 = animation.FuncAnimation(fig, animate, frames=100, fargs=(axs[0], x_List, y0_List, ser, b'tc0'), interval=1000) #, save_count=MAX_FRAMES
 
-# animate_all_plots(i,ser, 
-#                       ax0, x0_List, y0_List, command0_to_send_in_serial,
-#                       ax1, x1_List, y1_List, command1_to_send_in_serial,
-#                       ax2, x2_List, y2_List, command2_to_send_in_serial): 
+# ani_1 = animation.FuncAnimation(fig, full_animation, frames=100, fargs=(axs[0], axs[1], x_List, y0_List, y1_List, y2_List ser, b'tc0', b'tc1', b'tc2'), interval=1000) #, save_count=MAX_FRAMES
 
-ani = animation.FuncAnimation(fig, animate_all_plots, frames=100, fargs=(ser,
-                                                                         axs[0], x0_List, y0_List, b'tc0',
-                                                                         axs[1], x1_List, y1_List, b'tc1',
-                                                                         axs[2], x2_List, y2_List, b'tc2',), interval=1000)
+# ani = animation.FuncAnimation(fig, animate_all_plots, frames=100, fargs=(ser, x_List,
+#                                                                          axs[0], y0_List, b'tc0',
+#                                                                          axs[1], y1_List, b'tc1',
+#                                                                          axs[2], y2_List, b'tc2',), interval=1000)
 app.mainloop()
 ser.close()
 print("Serial is closed")

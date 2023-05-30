65acb56bce713e025ca9c844c3ca452e09644069
Simeon Popov <constructor_popovS@gmail.com>
2023-05-30 09:46:53 +0100

    writingToFile is working but need some correction

--- AnimationApp.py
+++ AnimationApp.py
@@ -1,34 +1,89 @@
 import matplotlib.animation as animation
 import DataReader
+import csv #for saving data in csv format file
+import time #for saving time of each measurement
 
 class AnimationApp():
 
     def __init__(self, fig, axs, controller):
         self.animation_function = animation.FuncAnimation(fig, self.animate, frames=100, fargs=(axs, self), interval=1000)
         self.doAnimation_flag = False
+
         self.data_reader = DataReader.DataReader()
+
         self.x_list = []
         self.tc0_list = []
         self.tc1_list = []
         self.tc2_list = []
 
+        self.animation_is_going_flag = False
+        self.data_file = None
+        self.writer = None
+
+        self.start_time = float(0.0)
+        # self.sample_time = float(0.0)
+        # self.sample_time_from_start = float(0.0)
+
     def animate(self, i, axs, controller): 
         if (self.doAnimation_flag == True):
-            print("Animation!")
-            new_tc0_value = self.data_reader.read_tc0()
-            new_tc1_value = self.data_reader.read_tc1()
-            new_tc2_value = self.data_reader.read_tc2()
-            
+
+            if (self.animation_is_going_flag == False):
+                self.animation_is_going_flag = True
+
+                self.data_file = open('new_data_file.txt', 'w')
+                self.writer = csv.writer(self.data_file)
+                print("File is created")
+
+                channels = (0, 1, 2)
+
+                # Display the header row for data table:
+                dataHeader = "    Sample,    "
+                dataHeader += "  Time,    "
+                for channel in channels:
+                    dataHeader += " Channel" + str(channel) + ","
+                
+                self.start_time = time.perf_counter()
+
+                print(dataHeader)
+                self.data_file.write(dataHeader + '\n')
+
+            number_of_measurement = None
             try:
                 number_of_measurement = self.x_list[-1] + 1
             except:
                 number_of_measurement = 0
             finally:
                 controller.x_list.append(number_of_measurement)
-    
+            
+            dataRow = []
+            dataRow.append(number_of_measurement)
+
+            # Display the updated number of measurement count
+            print('\r{:6d}'.format(number_of_measurement, end=''))
+
+            sample_time = time.perf_counter()
+            sample_time_from_start = sample_time - self.start_time
+            dataRow.append(sample_time_from_start)
+            print('{:12.2f} s'.format(sample_time_from_start), end='')
+
+            # Read a single value from each selected channel
+            new_tc0_value = self.data_reader.read_tc0()
+            dataRow.append(new_tc0_value)
+            print('{:12.2f} C'.format(new_tc0_value), end='')
+
+            new_tc1_value = self.data_reader.read_tc1()
+            dataRow.append(new_tc1_value)
+            print('{:12.2f} C'.format(new_tc1_value), end='')
+
+            new_tc2_value = self.data_reader.read_tc2()
+            dataRow.append(new_tc2_value)
+            print('{:12.2f} C'.format(new_tc2_value), end='')
+
             self.tc0_list.append(new_tc0_value)
             self.tc1_list.append(new_tc1_value)
             self.tc2_list.append(new_tc2_value)
+
+            self.writer.writerow(dataRow)
     
             self.x_list = controller.x_list[-10:]
     
@@ -60,8 +115,7 @@ class AnimationApp():
             #     axs.set_xlim(0, 10)
     
             # 
-        else:
-            print("Animation is: " + str(self.doAnimation_flag))
+        # print("Animation_flag is: " + str(self.doAnimation_flag))
 
     def resumeAnimation(self):
         self.doAnimation_flag = True
@@ -73,7 +127,9 @@ class AnimationApp():
 
     def stopAnimation(self):
         self.doAnimation_flag = False
+        self.animation_is_going_flag = False
         try:
+            self.data_file.close()
             self.animation_function.pause()
             print("Stop")
         except:
